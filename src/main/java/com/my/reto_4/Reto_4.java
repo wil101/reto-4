4d97cb10b37b47ab0d0a89a01eefe8ecb01644d3
wil101 <101955537+wil101@users.noreply.github.com>
2022-06-22 17:01:18 -0500

    se mota, panel, seguro,main

---
+++ src/main/java/com/my/reto_4/Reto_4.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.my.reto_4;
+
+import java.text.ParseException;
+
+/**
+ *
+ * @author wilmar.osorio1
+ */
+public class Reto_4 {
+
+    public static void main(String[] args) throws ParseException {
+        System.out.println("Hello World!");
+        seguro a = new seguro();
+        a.prueba();
+    }
+}
---
+++ src/main/java/com/my/reto_4/seguro.java
@@ -0,0 +1,44 @@
+
+package com.my.reto_4;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ *
+ * @author wilmar.osorio
+ */
+public class seguro {
+    String nombreEmpresa,descripcion, fechaInicial, fechaFinal;
+    int id;
+
+    public seguro(String nombreEmpresa, String descripcion, int id, String fechaInicial, String fechaFinal) {
+        this.nombreEmpresa = nombreEmpresa;
+        this.descripcion = descripcion;
+        this.id = id;
+        this.fechaInicial = fechaInicial;
+        this.fechaFinal = fechaFinal;
+    }
+    
+   public int getNumDiasCobertura() throws ParseException{
+       SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+       Date a = dateFormat.parse(fechaInicial);
+       Date b = dateFormat.parse(fechaFinal);
+       int dias = (int) ((b.getTime() - a.getTime()) / 86400000);
+       return dias;
+   }
+   
+   public seguro(){
+       
+   }
+   
+   public void prueba() throws ParseException{
+       SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+       Date a = dateFormat.parse("2022-02-14");
+       Date b = dateFormat.parse("2022-02-23");
+       int dias = (int) ((b.getTime() - a.getTime()) / 86400000);
+       System.out.println(dias);
+   }
+   
+}
